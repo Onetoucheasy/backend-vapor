389314cab3e4764ddb78fc8533deb3df96ecdba1
Eric O <ejolsson@users.noreply.github.com>
2023-07-28 12:50:24 +0200

    Agregar archivos iniciales

---
+++ RestauranteOfertasServer/Package.swift
@@ -0,0 +1,31 @@
+// swift-tools-version:5.8
+import PackageDescription
+
+let package = Package(
+    name: "RestauranteOfertasServer",
+    platforms: [
+       .macOS(.v13)
+    ],
+    dependencies: [
+        // 💧 A server-side Swift web framework.
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.77.1"),
+        // 🗄 An ORM for SQL and NoSQL databases.
+        .package(url: "https://github.com/vapor/fluent.git", from: "4.8.0"),
+        // ὁ8 Fluent driver for Postgres.
+        .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.7.2"),
+    ],
+    targets: [
+        .executableTarget(
+            name: "App",
+            dependencies: [
+                .product(name: "Fluent", package: "fluent"),
+                .product(name: "FluentPostgresDriver", package: "fluent-postgres-driver"),
+                .product(name: "Vapor", package: "vapor")
+            ]
+        ),
+        .testTarget(name: "AppTests", dependencies: [
+            .target(name: "App"),
+            .product(name: "XCTVapor", package: "vapor"),
+        ])
+    ]
+)
